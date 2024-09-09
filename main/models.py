28bb687546a1cbd2939f3f483b70575dc38212ce
mdlnecg <mdlnecg@gmail.com>
2024-09-09 22:34:04 +0700

    Update web

--- main/models.py
+++ main/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-class MoodEntry(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
+    image_url = models.URLField(max_length=200, blank=True, null=True)  # Field untuk URL gambar produk
+
+    def __str__(self):
+        return self.name

42ddb23be9686dcfc1faa9892ad78d150beb048a
mdlnecg <mdlnecg@gmail.com>
2024-09-09 23:25:06 +0700

    Update web

--- main/views.py
+++ main/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
 
+# Create your views here.
 def show_main(request):
-    products = [
-        {
-            'name': 'Chocolate CooKIEZZ',
-            'description': 'Delicious chocolate cookie with a gooey center.',
-            'price': 10.000,
-            'image_url': '"C:\Users\Madeline\Documents\pacil\TERM 3\PBP\PICTS\chocoKIEZZ.jpg"'
-        },
-        {
-            'name': 'Peanut Butter Cookie',
-            'description': 'Rich peanut butter cookie with a crunchy exterior.',
-            'price': 18000,
-            'image_url': '"C:\Users\Madeline\Documents\pacil\TERM 3\PBP\PICTS\RVKIEZZ.jpg"'
-        },
-        # Tambahkan produk lainnya
-    ]
-
     context = {
-        'name': 'LaRizzManizz',
-        'shop_at': 'Your Favorite Online Cookie Store',
-        'products': products,
+        'name' : 'Niki Zefanya',
+        'shop_at': 'LaRizzManizz',
+        'products': 'Chocolate CooKIEZZ'
     }
 
-    return render(request, 'main.html', context)
+    return render(request, "main.html", context)

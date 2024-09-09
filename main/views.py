6ca3bdce55dae6df2a11463ea01fbb2e6053b814
mdlnecg <mdlnecg@gmail.com>
2024-09-09 15:19:30 +0700

    Updates for Tugas 2 PBP

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'name' : 'Niki Zefanya',
+        'shop_at': 'LaRizzManizz',
+        'products': 'Glasses'
+    }
+
+    return render(request, "main.html", context)

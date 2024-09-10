75192cf073148ebc25b5a0db7a3f033f9be34047
mdlnecg <mdlnecg@gmail.com>
2024-09-10 17:25:19 +0700

    Update main.html and views.py

--- main/views.py
+++ main/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def show_main(request):
+    # Menggunakan data produk dari context (sementara hardcoded)
     context = {
-        'name' : 'Chocolate CoKIEZZ',
-        'price': '10.000',
-        'description': 'The only cooKIEZZ that brings happiNEZZ!'
+        'products': [
+            {
+                'name': 'Chocolate CoKIEZZ',
+                'price': '10.000',
+                'description': 'The only cooKIEZZ that brings happiNEZZ!',
+                'rating': '⭐⭐⭐⭐⭐',
+            },
+            {
+                'name': 'Red Velvet CoKIEZZ',
+                'price': '12.000',
+                'description': 'A KIEZZ from 7th Heaven!',
+                'rating': '⭐⭐⭐⭐',
+            },
+            {
+                'name': 'Oreo CoKIEZZ',
+                'price': '10.000',
+                'description': 'RUN from skibidi, GET your Oreozzz!!',
+                'rating': '⭐⭐⭐⭐',
+            }
+        ]
     }
 
     return render(request, "main.html", context)

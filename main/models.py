39438a47691222ab8228cd40e88cadae7e849b67
mdlnecg <mdlnecg@gmail.com>
2024-09-18 00:48:58 +0700

    Updates for Tugas 3 PBP

--- main/models.py
+++ main/models.py
@@ -1,10 +1,12 @@
+import uuid
 from django.db import models
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
-    image_url = models.URLField(max_length=200, blank=True, null=True)  # Field untuk URL gambar produk
+    rating = models.CharField(max_length=5, default='⭐⭐⭐⭐')
 
     def __str__(self):
         return self.name
--- main/views.py
+++ main/views.py
@@ -1,33 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.core import serializers
 from .models import Product
+from main.forms import ProductEntryForm
 
 # Create your views here.
-def show_main(request):
-    # Menggunakan data produk dari context (sementara hardcoded)
+def show_model(request):
+    model = Product.objects.all()
+
     context = {
         'name' : 'Madeline Clairine Gultom',
         'npm' : '2306207846',
         'class' : 'PBP D',
-        'products': [
-            {
-                'name': 'Chocolate CoKIEZZ',
-                'price': '10.000',
-                'description': 'The only cooKIEZZ that brings happiNEZZ!',
-                'rating': '⭐⭐⭐⭐⭐',
-            },
-            {
-                'name': 'Red Velvet CoKIEZZ',
-                'price': '12.000',
-                'description': 'A KIEZZ from 7th Heaven!',
-                'rating': '⭐⭐⭐⭐',
-            },
-            {
-                'name': 'Oreo CoKIEZZ',
-                'price': '10.000',
-                'description': 'RUN from skibidi, GET your Oreozzz!!',
-                'rating': '⭐⭐⭐⭐',
-            }
-        ]
+        'products': model,
     }
 
     return render(request, "main.html", context)
+
+def create_product_form(request):
+    form = ProductEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_model')
+
+    context = {'form': form}
+    return render(request, 'create_product.html', context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")

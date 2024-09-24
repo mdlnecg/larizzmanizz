4260ce6f572fd98c5eea71d292859fb57e1a9a99
mdlnecg <mdlnecg@gmail.com>
2024-09-24 20:23:21 +0700

    Fix commit and updates for Tugas 4 PBP

--- larizzmanizz/settings.py
+++ larizzmanizz/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oj7il^durl%31i896e7b777jk@58cegdev93&39xeiq$+4jvvx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "madeline-clairine-larizzmanizz.pbp.cs.ui.ac.id"]
 
